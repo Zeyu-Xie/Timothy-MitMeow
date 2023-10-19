7b14a7ff5e76033b6ee9665a5de96680d79adf2a
Acan <xie.zeyu20@gmail.com>
2023-10-19 14:36:01 +0800

    .

---
+++ Timothy MitMeow/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Timothy MitMeow
+//
+//  Created by Acan on 2023/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+            Image("test")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Timothy MitMeow/Timothy_MitMeowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Timothy_MitMeowApp.swift
+//  Timothy MitMeow
+//
+//  Created by Acan on 2023/10/19.
+//
+
+import SwiftUI
+
+@main
+struct Timothy_MitMeowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
